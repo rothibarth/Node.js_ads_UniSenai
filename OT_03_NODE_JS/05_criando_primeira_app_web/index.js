856e3467960d0429baf54dfeedead9479754296e
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2024-06-06 21:30:16 -0300

    atualizando 0T 03 NODEJS

---
+++ OT_03_NODE_JS/05_criando_primeira_app_web/index.js
@@ -0,0 +1,11 @@
+//Nesta aula veremos como proceder para criar nossa primeira aplicação web com Node,js
+//Para isso, utilizaremos o pacote HTTP para criar um servidor e acessá-lo por meio do browser.
+
+var http = require("http");
+
+http.createServer(function(request, response){
+    response.write("Primeiros passos com Node.JS");
+    responde.end();
+}).listen(8081);
+
+console.log("Servidor rodando em http://localhost:8081");
