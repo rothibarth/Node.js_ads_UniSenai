a0fb1d3c7c39b50dd33a7605e65c66a019039365
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2024-06-10 20:52:31 -0300

    atualizando a ot_03_node

--- OT_03_NODE_JS/05_criando_primeira_app_web/index.js
+++ OT_03_NODE_JS/05_criando_primeira_app_web/index.js
@@ -5,7 +5,7 @@ var http = require("http");
 
 http.createServer(function(request, response){
     response.write("Primeiros passos com Node.JS");
-    responde.end();
+    response.end();
 }).listen(8081);
 
 console.log("Servidor rodando em http://localhost:8081");
