12e002c4dea62c828b0a462184dcf6b27bad81e1
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2024-06-12 19:22:39 -0300

    atualizando OT 04 NODE

--- OT_04_NODE_JS/http/index.js
+++ OT_04_NODE_JS/http/index.js
@@ -9,3 +9,19 @@ console.log('Servidor iniciado na porta 3000')
 app.get('/', (req, res) =>
 res.send('<h1 style="color: blue">CRIANDO UM SERVIDOR COM EXPRESS.JS</h1>')
 );
+
+app.get('/sobre', (req, res) =>
+res.send('<h1 style="color: blue">PÁGINA SOBRE</h1>')
+);
+
+app.get('/contato', (req, res) =>
+res.send('<h1 style="color: blue">PÁGINA CONTATO</h1>')
+);
+
+app.get('/empresa', (req, res) =>
+res.send('<h1 style="color: blue">PÁGINA EMPRESA</h1>')
+);
+
+
+
+
