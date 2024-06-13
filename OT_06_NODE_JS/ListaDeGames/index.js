a1276956e5d554bc023f5e08a333d63a1ffef24a
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2024-06-13 20:53:55 -0300

    atualizando a OT 06 NODE

--- OT_06_NODE_JS/ListaDeGames/index.js
+++ OT_06_NODE_JS/ListaDeGames/index.js
@@ -29,3 +29,18 @@ app.listen(3080, () =>{
 app.get("/", (req, res) =>{
     res.json(games);
 })
+
+app.use(express.json());
+
+app.post("/novogame", (req, res) =>{
+    let title = req.body.title;
+    let studio = req.body.studio;
+    let price = req.body.price;
+
+    console.log(title);
+    console.log(studio);
+    console.log(price); 
+
+    res.send("OK");
+
+});
