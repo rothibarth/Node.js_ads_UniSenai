6009834e9acb419dd1c0535e7bd1addf6d42d046
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2024-06-13 20:40:03 -0300

    Atualizando a OT 06 NODE

---
+++ OT_06_NODE_JS/ListaDeGames/index.js
@@ -0,0 +1,31 @@
+//carregando o express
+const express = require("express");
+//instancio o express e carregando a biblioteca do express dentro dessa const app
+const app = express();
+
+//Lista de Games
+
+let games = [
+    {title: "Sea of Thieves", studio: "Rare", price: 30},
+    {title: "WOW", studio: "Blizzard", price: 120},
+    {title: "Valorant", studio: "Riot", price: 0},
+    {title: "COD", studio: "Activision", price: 200},
+    {title: "Minecraft", studio: "Mojang", price: 80},
+    {title: "Zelda", studio: "Nintendo", price: 59},
+    {title: "Red Dead", studio: "Rockstar", price: 60},
+    {title: "The Witcher", studio: "CD Projekt Red", price: 40},
+    {title: "Cyberpunk 2077", studio: "CD Projekt Red", price: 60},
+    {title: "God of War", studio: "Santa Monica Studio", price: 39},
+    {title: "Among Us", studio: "Innersloth", price: 05},
+    {title: "Fortnite", studio: "Epic Games", price: 0},
+    {title: "Grand Theft Auto V", studio: "Rockstar Games", price: 30}
+
+]
+
+app.listen(3080, () =>{
+    console.log("Servidor rodando!");
+})
+
+app.get("/", (req, res) =>{
+    res.json(games);
+})
