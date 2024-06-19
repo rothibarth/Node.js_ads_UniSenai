5bc7ca778bb8451dabcc59768365b112b712dfcc
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2024-06-19 20:58:26 -0300

    Atualizando OT 09 NODE

--- OT_09_NODE_JS/HTTP/index.js
+++ OT_09_NODE_JS/HTTP/index.js
@@ -2,6 +2,11 @@ import Express from 'Express';
 
 const app = Express();
 
+app.get('/', (req, res) =>
+res.send("<h3>Rotas no Express</h3><p>Rota '/'")
+);
+
 app.listen(3000, () =>
 console.log('Servidor iniciado na porta 3000')
 );
+
