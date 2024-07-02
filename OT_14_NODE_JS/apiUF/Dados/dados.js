f604302782f48936810c32b94655140345560bc6
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2024-07-02 19:21:04 -0300

    Atualizando OT 14 de Node

---
+++ OT_14_NODE_JS/apiUF/Dados/dados.js
@@ -0,0 +1,164 @@
+const colecaoUf = [
+    {
+        id: 1,
+        uf: "AC",
+        nome: "Acre"
+    },
+    {
+        id: 2,
+        uf: "AL",
+        nome: "Alagoas"
+    },
+    {
+        id: 3,
+        uf: "AM",
+        nome: "Amazonas"
+    },
+    {
+        id: 4,
+        uf: "AP",
+        nome: "Amapá"
+    },
+    {
+        id: 5,
+        uf: "BA",
+        nome: "Bahia"
+    },
+    {
+        id: 6,
+        uf: "CE",
+        nome: "Ceará"
+    },
+    {
+        id: 7,
+        uf: "DF",
+        nome: "Distrito Federal"
+    },
+    {
+        id: 8,
+        uf: "ES",
+        nome: "Espírito Santo"
+    },
+    {
+        id: 9,
+        uf: "GO",
+        nome: "Goiás"
+    },
+    {
+        id: 10,
+        uf: "MA",
+        nome: "Maranhão"
+    },
+    {
+        id: 11,
+        uf: "MG",
+        nome: "Minas Gerais"
+    },
+    {
+        id: 12,
+        uf: "MS",
+        nome: "Mato Grosso do Sul"
+    },
+    {
+        id: 13,
+        uf: "MT",
+        nome: "Mato Grosso"
+    },
+    {
+        id: 14,
+        uf: "PA",
+        nome: "Pará"
+    },
+    {
+        id: 15,
+        uf: "PB",
+        nome: "Paraíba"
+    },
+    {
+        id: 16,
+        uf: "PE",
+        nome: "Pernambuco"
+    },
+    {
+        id: 17,
+        uf: "PI",
+        nome: "Piauí"
+    },
+    {
+        id: 18,
+        uf: "PR",
+        nome: "Paraná"
+    },
+    {
+        id: 19,
+        uf: "RJ",
+        nome: "Rio de Janeiro"
+    },
+    {
+        id: 20,
+        uf: "RN",
+        nome: "Rio Grande do Norte"
+    },
+    {
+        id: 21,
+        uf: "RO",
+        nome: "Rondônia"
+    },
+    {
+        id: 22,
+        uf: "RR",
+        nome: "Roraima"
+    },
+    {
+        id: 23,
+        uf: "RS",
+        nome: "Rio Grande do Sul"
+    },
+    {
+        id: 24,
+        uf: "SC",
+        nome: "Santa Catarina"
+    },
+    {
+        id: 25,
+        uf: "SE",
+        nome: "Sergipe"
+    },
+    {
+        id: 26,
+        uf: "SP",
+        nome: "São Paulo"
+    },
+    {
+        id: 27,
+        uf: "TO",
+        nome: "Tocantins"
+    }]
+
+
+    export default colecaoUf;
+
+//Exemplo da OT --------------------------
+// const colecaoTimes = [
+//     {
+//         id: 1,
+//         nome: "Flamengo"
+//     },
+
+//     {
+//         id: 2,
+//         nome: "Atletico Mineiro"
+//     },
+
+//     {
+//         id: 3,
+//         nome: "Palmeiras"
+//     },
+
+//     {
+//         id: 4,
+//         nome: "São Paulo"
+//     },
+
+
+// ]
